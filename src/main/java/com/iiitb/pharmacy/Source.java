81f4d0e746b4f470bce0df98bffc73936144a33d
Meghna Dubey <meghnadubey300@gmail.com>
2021-11-15 13:30:23 +0530

    project created and configured

---
+++ src/main/java/com/iiitb/pharmacy/Source.java
@@ -0,0 +1,26 @@
+package com.iiitb.pharmacy;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class Source {
+    @Bean
+    public DataSource getdataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
+        String url = System.getenv("DATABASE_HOST");
+        if (url != null) {
+            dataSourceBuilder.url("jdbc:mysql://mysql-db-tgl:3306/pms?createDatabaseIfNotExist=true&useSSL=false&allowPublicKeyRetrieval=true&autoReconnect=true&failOverReadOnly=false&maxReconnects=10");
+        } else {
+            dataSourceBuilder.url("jdbc:mysql://localhost:3306/pms?createDatabaseIfNotExist=true");
+        }
+        dataSourceBuilder.username("root");
+        dataSourceBuilder.password("euphoria19");
+        return dataSourceBuilder.build();
+    }
+}
+
