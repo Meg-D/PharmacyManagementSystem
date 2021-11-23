59cf985c7d85a2a9171b93c1ac55c635884044a4
Meghna Dubey <meghnadubey300@gmail.com>
2021-11-23 19:09:48 +0530

    request apis corrected

--- src/main/java/com/iiitb/pharmacy/controller/RequestController.java
+++ src/main/java/com/iiitb/pharmacy/controller/RequestController.java
@@ -9,32 +9,33 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/request")
 public class RequestController {
 
     @Autowired
     private RequestService requestService;
 
     //add request
-    @PostMapping(path="/request",consumes = "application/JSON")
+    @PostMapping(path="/add",consumes = "application/JSON")
     public Request addRequest(@RequestBody Request request){
         return this.requestService.addRequest(request);
     }
 
     //get all requests
-    @GetMapping("/requests")
+    @GetMapping("/getall")
     public List<Request> getRequests(){
         return this.requestService.getRequests();
     }
 
     //get request by id
-    @GetMapping("/request/{id}")
+    @GetMapping("/getbyid/{id}")
     public Request getRequestById(@PathVariable String id){
         return this.requestService.getRequestById(Integer.parseInt(id));
     }
 
     //update request
-    @PutMapping(path= "/request/{id}",consumes = "application/JSON")
+    @PutMapping(path= "/update",consumes = "application/JSON")
     public Request updateRequest(@RequestBody Request request){
         return this.requestService.updateRequest(request);
     }
